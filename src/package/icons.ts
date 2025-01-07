6e1a07bf9854eeaaa3186d755b88bedc6a60cdfe
nasa14119 <nico.amaya0205@gmail.com>
2025-01-07 14:46:43 -0600

    Adding export for tipe objet

--- src/package/icons.ts
+++ src/package/icons.ts
@@ -1,23 +1,32 @@
-function camelToKebabWithNumbers(text: string) {
-  return text
-    .replace(/([a-z])([A-Z0-9])/g, "$1-$2") // De letra minúscula a mayúscula o número
-    .replace(/([0-9])([a-zA-Z])/g, "$1-$2") // De número a letra
-    .toLowerCase();
-}
 import * as remix_icons from "@remixicon/react";
 
-const parse_content = [];
-const content = Object.entries(remix_icons);
 export type RemixIcon = {
   key: string;
   Component: remix_icons.RemixiconComponentType;
 };
-for (let i = 0; i < content.length; i++) {
-  const key = content[i][0].slice(2);
-  parse_content.push({
-    key,
-    Component: content[i][1],
-  });
+export type RemixIconObject = {
+  [key: string]: remix_icons.RemixiconComponentType;
+};
+function iconsList() {
+  const parse_content = [];
+  const content = Object.entries(remix_icons);
+  for (let i = 0; i < content.length; i++) {
+    const key = content[i][0].slice(2);
+    parse_content.push({
+      key,
+      Component: content[i][1],
+    });
+  }
+  return parse_content;
 }
-
-export const RemixIcons = [...parse_content] as RemixIcon[];
+function iconsObject() {
+  const parse_content: RemixIconObject = {};
+  const content = Object.entries(remix_icons);
+  for (let i = 0; i < content.length; i++) {
+    const key = content[i][0].slice(2);
+    parse_content[key] = content[i][1];
+  }
+  return parse_content;
+}
+export const RemixIconsList = iconsList() as RemixIcon[];
+export const RemixIconsObject = iconsObject();
