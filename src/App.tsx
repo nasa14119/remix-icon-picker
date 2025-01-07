49bf2ef0777fbd4443cd69bcca83443189f84967
nasa14119 <nico.amaya0205@gmail.com>
2025-01-06 18:07:43 -0600

    reordering directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import "./App.css";
-import { icons } from "./package/icons";
-import { StaticGrid } from "./package/staticGrid";
+import { RemixIcons } from "./package/icons";
 function App() {
-  console.log(icons);
-  return (
-    <main>
-      <StaticGrid
-        className="grid items-center justify-center grid-cols-[repeat(20,_max-content)] h-screen p-10 max-h-full overflow-scroll"
-        current_selected="apps-2-ai-fill"
-        classNameIcons="hover:bg-slate-200/20 hover:text-white/20 hover:rounded-lg p-2"
-        isSelected="text-red-500"
-      />
-    </main>
-  );
+  console.log(RemixIcons);
+  return <main></main>;
 }
 
 export default App;
