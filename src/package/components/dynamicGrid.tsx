436a5e5a12a5d4cf47be15e3aef504cd02a20f92
nasa14119 <nico.amaya0205@gmail.com>
2025-01-07 14:46:22 -0600

    adding memo for not rendering if Icons are the same

--- src/package/components/dynamicGrid.tsx
+++ src/package/components/dynamicGrid.tsx
@@ -1,13 +1,13 @@
-import { HTMLAttributes } from "react";
-import type { RemixIcon } from "./icons.ts";
-import { cn } from "./lib/utils";
+import { HTMLAttributes, memo } from "react";
+import type { RemixIcon } from "../icons.ts";
+import { cn } from "../lib/utils.ts";
 import { ClassValue } from "clsx";
 interface Props extends HTMLAttributes<HTMLDivElement> {
   Icons: RemixIcon[];
   classNameIcons?: ClassValue;
 }
 
-export const DynamicGrid = ({
+const DynamicGridComponent = ({
   Icons,
   className,
   classNameIcons,
@@ -15,12 +15,13 @@ export const DynamicGrid = ({
   ...props
 }: Props) => {
   return (
-    <div className={cn([className])} {...props}>
+    <div className={cn(["flex flex-wrap", className])} {...props}>
       {Icons.map(({ Component, key }) => (
-        <span className={cn([classNameIcons])} onClick={onClick}>
-          <Component key={key} />
+        <span className={cn([classNameIcons])} onClick={onClick} key={key}>
+          <Component />
         </span>
       ))}
     </div>
   );
 };
+export const DynamicGrid = memo(DynamicGridComponent);
