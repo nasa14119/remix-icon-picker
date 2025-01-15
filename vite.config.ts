171b2522b3d4bd472ba4c95507ab065588936914
nasa14119 <nico.amaya0205@gmail.com>
2025-01-15 13:17:23 -0600

    Adding alias @lib @components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@lib": path.resolve("src/package/lib"),
+      "@components": path.resolve("src/package/components"),
+    },
+  },
   plugins: [react()],
-})
+});
