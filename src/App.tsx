7137727c944c4e81d632a837f493ec525e296e76
nasa14119 <nico.amaya0205@gmail.com>
2025-01-16 18:31:05 -0600

    changes in the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ function App() {
   return (
     <main className="flex flex-col gap-y-10 m-10">
       <StaticPicker>
-        <h1 className="my-5">Remix Picker</h1>
+        <h1 className="mt-5 text-5xl">Remix Picker</h1>
         <div className="sticky top-2 flex items-center z-50">
           <StaticPicker.InputText />
           <StaticPicker.CurrentIcon
+            legend
             defaultIcon="ArrowLeftRightLine"
-            className="text-black"
-            classNameBg="bg-white opacity-95"
+            className="text-black group bg-white hover:scale-[1.1] transition-all"
+            classNameBg="hidden"
+            classNameLegend="absolute -bottom-10 right-0 text-black bg-white p-2 rounded-xl group-hover:visible invisible"
           />
         </div>
-        <StaticPicker.Grid className="gap-5 justify-between relative z-10">
+        <StaticPicker.Grid className="md:gap-5 justify-between relative z-10">
           <StaticPicker.Icon isFoundClass="bg-red-700/30 text-red-700 transition-all duration-300"></StaticPicker.Icon>
         </StaticPicker.Grid>
       </StaticPicker>
